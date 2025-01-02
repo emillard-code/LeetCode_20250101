11d3b8646af6c28e1f01386660bc435ad6fb743c
Emerson Millard <emillard.code@gmail.com>
2025-01-02 12:00:00 -0800

    Initial commit.

---
+++ src/test/java/com/project/MainTest.java
@@ -0,0 +1,15 @@
+package com.project;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+
+    @Test
+    public void testMain() {
+
+        assertTrue(true);
+
+    }
+
+}
