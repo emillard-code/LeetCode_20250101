9b9bf686ce7b8234ecdb4d1995c5885206c0befd
Emerson Millard <emillard.code@gmail.com>
2025-01-02 12:00:00 -0800

    Added challenge solution logic.

--- src/main/java/com/project/solution/LeetCodeSolution.java
+++ src/main/java/com/project/solution/LeetCodeSolution.java
@@ -1,4 +1,37 @@
 package com.project.solution;
 
 public class LeetCodeSolution {
+
+    public static void main(String[] args) {
+
+        System.out.println(maximumScoreAfterSplittingAString("011101"));
+        System.out.println(maximumScoreAfterSplittingAString("00111"));
+        System.out.println(maximumScoreAfterSplittingAString("1111"));
+
+    }
+
+    public static int maximumScoreAfterSplittingAString(String number) {
+
+        int solution = 0;
+
+        for (int i = 0; i < number.length() - 1; i++) {
+
+            int current = 0;
+
+            for (int j = 0; j <= i; j++) {
+                if (number.charAt(j) == '0') { current++; }
+            }
+
+            for (int j = i + 1; j < number.length(); j++) {
+                if (number.charAt(j) == '1') { current++; }
+            }
+
+            solution = Math.max(solution, current);
+
+        }
+
+        return solution;
+
+    }
+
 }
